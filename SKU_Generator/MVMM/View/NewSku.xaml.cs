78408d05fcfb25b3c24ff72176cd1c3ec3dd025f
AkhilMohan <AkhilMohan@SOCIUS>
2023-01-20 10:36:24 +0530

    UI basic

---
+++ SKU_Generator/MVMM/View/NewSku.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SKU_Generator.MVMM.View
+{
+    /// <summary>
+    /// Interaction logic for NewSku.xaml
+    /// </summary>
+    public partial class NewSku : UserControl
+    {
+        private ObservableCollection<MyItem> myItems = new ObservableCollection<MyItem>();
+        public NewSku()
+        {
+            InitializeComponent();
+            myListBox.ItemsSource = myItems;
+            myListBox1.ItemsSource = myItems;
+            myListBox2.ItemsSource = myItems;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newItem = new MyItem { Name = "New Item", IsChecked = false };
+
+            // Add the item to the collection
+            myItems.Add(newItem);
+
+            // Refresh the ListBox to display the new item
+            myListBox.Items.Refresh();
+        }
+    }
+    public class MyItem
+    {
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+    }
+}
---
+++ SKU_Generator/MVMM/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SKU_Generator.Core;
+
+namespace SKU_Generator.MVMM.ViewModel
+{
+
+        class MainViewModel : ObservableObject
+        {
+           
+            public RelayCommand SkuViewCommand { get; set; }
+           
+
+            public NewSku NewSku { get; set; }
+
+          
+
+            private object _currentView;
+
+
+            public object CurrentView
+            {
+                get { return _currentView; }
+                set
+                {
+                    _currentView = value;
+                    OnPropertyChanged();
+                }
+            }
+            public MainViewModel()
+            {
+            
+              NewSku = new NewSku();
+               
+
+                CurrentView = NewSku;
+
+            //HomeViewCommand = new RelayCommand(o =>
+            //{
+            //    CurrentView = HomeVm;
+            //});
+
+            SkuViewCommand = new RelayCommand(o =>
+                {
+                    CurrentView = NewSku;
+                });
+
+               
+
+            }
+        }
+    }
+
